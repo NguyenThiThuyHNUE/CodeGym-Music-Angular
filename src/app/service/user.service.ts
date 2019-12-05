08e1b966833f70b65cdc156b6e43539acb935a09
Do Hanh <dovanhanh12b@gmail.com>
2019-12-05 16:12:54 +0700

    register-client-modal

---
+++ src/app/service/user.service.ts
@@ -0,0 +1,15 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Url} from "../../../url-project";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  constructor(private http: HttpClient) {
+  }
+
+  userRegister(info) {
+    return this.http.post(Url + '/api/register', info)
+  }
+}
---
+++ url-project.ts
@@ -0,0 +1,3 @@
+'use strict';
+
+export const Url = 'http://localhost:8000';
