4c93b5fb8f1365ea1d06648dcfe2098f67d551df
Hanh <dovanhanh12b@gmail.com>
2019-12-19 10:03:15 +0700

    ..

--- src/app/user/body/main/main.component.ts
+++ src/app/user/body/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-main',
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+  isPlay: number = 1;
+  showVolume: boolean = false;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  playMusic() {
+    if (this.isPlay == 0) {
+      return this.isPlay = 1;
+    }
+    return this.isPlay = 0;
+  }
+
+  volumeShow() {
+    return this.showVolume = true
+  }
+
+  volumeHide() {
+    return this.showVolume = false
+  }
 }
